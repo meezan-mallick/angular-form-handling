7e270a06e9ef081f23bda66c09d1e4a7ad02581a
meezan-mallick <meezan.mallick@gmail.com>
2021-01-20 23:10:25 +0530

    state manage and field validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form-handling';
+  @ViewChild("form") form:NgForm;
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+
+  onSubmit() {
+    console.log(this.form);
+  }
 }
